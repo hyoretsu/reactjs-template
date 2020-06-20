b291e05889563ff8516dee42df43dc72f0e5c4b7
Aran Leite <hyoretsu@gmail.com>
2020-06-19 23:24:02 -0300

    Added scroll retention

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { wrapHistory } from 'oaf-react-router';
 
 import GlobalStyle from './styles/global';
+import Fonts from './assets/fonts';
 import Routes from './routes';
 
-const App: React.FC = () => (
- <>
-  <BrowserRouter>
-   <Routes />
-  </BrowserRouter>
-  <GlobalStyle />
- </>
-);
+const App: React.FC = () => {
+ const history = createBrowserHistory();
+ wrapHistory(history);
+
+ return (
+  <>
+   <Router history={history}>
+    <Routes />
+   </Router>
+   <GlobalStyle />
+   <Fonts />
+  </>
+ );
+};
 
 export default App;
