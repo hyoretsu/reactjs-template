70c4aa803804b7b095a2e37b3434d2216f6511e3
Aran Leite <hyoretsu@gmail.com>
2020-06-20 14:34:45 -0300

    Implemented <Suspense> and error/loading screens

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Suspense} from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Loading from '../pages/Loading';
+import Error from '../pages/Error';
+
 const Routes: React.FC = () => (
- <Switch>
- </Switch>
+ <Suspense fallback={Loading}>
+  <Switch>
+   <Route component={Error} />
+  </Switch>
+ </Suspense>
 );
 
 export default Routes;
