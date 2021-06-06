d43f8c447a0ce0f24219f1c1716fe20d531f04df
Aran Leite <hyoretsu@gmail.com>
2021-06-06 14:31:20 -0300

    Final React Boilerplate

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,128 +1,118 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { gzip } from '@gfx/zopfli';
-import CompressionPlugin from 'compression-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import path from 'path';
 import webpack from 'webpack';
-import { WebpackManifestPlugin } from 'webpack-manifest-plugin';
+import { CustomizeRule } from 'webpack-merge';
+import { mergeWithRules } from 'webpack-merge';
 
 import appPackageJson from './package.json';
+import TSConfig from './tsconfig.json';
 
-const config: webpack.Configuration = {
- cache: true,
- devtool: 'source-map',
- experiments: {
-  asset: true,
- },
- module: {
-  rules: [
-   {
-    test: /\.(j|t)sx?$/,
-    exclude: /node_modules/,
-    use: 'babel-loader',
-   },
-   {
-    test: /\.css$/,
-    use: [MiniCssExtractPlugin.loader, 'css-loader'],
-   },
-   {
-    test: /\.(jpe?g|png|gif)$/,
-    type: 'asset',
-   },
-   {
-    test: /\.svg$/,
-    use: '@svgr/webpack',
-   },
-  ],
- },
- optimization: {
-  splitChunks: {
-   cacheGroups: {
-    vendors: {
-     test: /node_modules\/.pnpm\/(?!react-icons.*\/).*/,
-     name: 'vendors',
-     chunks: 'all',
-    },
-    ui: {
-     test: /node_modules\/.pnpm\/(react-icons.*\/).*/,
-     name: 'ui',
-     chunks: 'all',
-    },
-   },
-  },
-  runtimeChunk: {
-   name: 'runtime-main',
-  },
- },
- output: {
-  path: path.resolve(__dirname, 'build'),
-  assetModuleFilename: 'static/media/[name].[hash:8][ext]',
-  chunkFilename: 'static/js/[name].[contenthash:8].chunk.js',
-  filename: 'static/js/[name].[contenthash:8].js',
-  publicPath: appPackageJson.homepage,
- },
- plugins: [
-  // @ts-ignore
-  new CopyPlugin({
-   patterns: [
-    {
-     from: 'public',
-     globOptions: {
-      ignore: ['**/index.html'],
-     },
-    },
-   ],
-  }),
-  new HtmlWebpackPlugin({
-   template: path.resolve(__dirname, 'public', 'index.html'),
-  }),
-  // @ts-ignore
-  new MiniCssExtractPlugin({
-   filename: 'static/css/[name].[contenthash:8].css',
-   chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
-  }),
-  // @ts-ignore
-  new WebpackManifestPlugin({
-   fileName: 'asset-manifest.json',
-   generate: (seed, files, entries) => {
-    const manifestFiles = files.reduce((accumulator, file) => {
-     // @ts-ignore
-     accumulator[file.name] = file.path;
+const getAliases = (): Record<string, string[]> => {
+ const aliasesObject = {};
 
-     return accumulator;
-    }, seed);
-    const entrypointFiles = entries.main.filter(filename => !filename.endsWith('.map'));
+ Object.entries(TSConfig.compilerOptions.paths).forEach(entry => {
+  const alias = entry[0].split('/*')[0];
+  const aliased = entry[1][0].split('/*')[0];
 
-    return {
-     files: manifestFiles,
-     entrypoints: entrypointFiles,
-    };
-   },
-  }),
-  // @ts-ignore
-  new CompressionPlugin({
-   algorithm: gzip,
-   deleteOriginalAssets: false,
-   filename: '[path][base].gz',
-   test: /\.(js|css|html|svg)$/i,
-  }),
-  // @ts-ignore
-  new CompressionPlugin({
-   algorithm: 'brotliCompress',
-   compressionOptions: {
-    level: 11,
-   },
-   deleteOriginalAssets: false,
-   filename: '[path][base].br',
-   test: /\.(js|css|html|svg)$/i,
-  }),
- ],
- resolve: {
-  extensions: ['.ts', '.tsx', '.js'],
-  modules: [path.resolve(__dirname, 'src'), 'node_modules'],
- },
+  Object.assign(aliasesObject, {
+   [alias]: path.resolve(__dirname, 'src', aliased),
+  });
+ });
+
+ return aliasesObject;
 };
 
-export default config;
+export default function webpackConfig(
+ mode: 'production' | 'development',
+ specialConfigs: webpack.Configuration,
+): webpack.Configuration {
+ const config = {
+  mode,
+  cache: {
+   type: 'filesystem',
+   version: appPackageJson.version.split('.')[0],
+  },
+  module: {
+   rules: [
+    {
+     test: /\.(js|ts)x?$/,
+     exclude: /node_modules/,
+     use: {
+      loader: 'babel-loader',
+      options: {
+       cacheCompression: false,
+       cacheDirectory: true,
+      },
+     },
+    },
+    {
+     test: /\.css$/,
+     use: [
+      mode === 'production'
+       ? {
+          loader: MiniCssExtractPlugin.loader,
+          options: { publicPath: appPackageJson.homepage },
+         }
+       : 'style-loader',
+      {
+       loader: 'css-loader',
+       options: { importLoaders: 1 },
+      },
+     ],
+     sideEffects: true,
+    },
+    {
+     test: /\.(jpe?g|png|gif)$/,
+     type: 'asset/resource',
+    },
+    {
+     test: /\.svg$/,
+     use: '@svgr/webpack',
+    },
+   ],
+   strictExportPresence: true,
+  },
+  output: {
+   path: path.resolve(__dirname, 'build'),
+  },
+  // @ts-ignore
+  plugins: [
+   new CopyPlugin({
+    patterns: [
+     {
+      from: 'public',
+      globOptions: {
+       ignore: ['**/index.html'],
+      },
+     },
+    ],
+   }),
+   mode === 'production' &&
+    new MiniCssExtractPlugin({
+     filename: 'static/css/[name].[contenthash:8].css',
+     chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+    }),
+  ].filter(Boolean),
+  resolve: {
+   alias: getAliases(),
+   extensions: ['.tsx', '.ts', '.json', '.js', '.jsx'],
+   modules: ['node_modules', path.resolve(__dirname, 'src')],
+  },
+ };
+
+ const mergingRules = {
+  module: {
+   rules: {
+    test: CustomizeRule.Match,
+    use: {
+     loader: CustomizeRule.Match,
+     options: CustomizeRule.Replace,
+    },
+   },
+  },
+ };
+
+ return mergeWithRules(mergingRules)(config, specialConfigs);
+}
